03ba1a33ca7cf01d0753d36924476fc1cc56eb62
mmarkman <mmarkman@users.noreply.github.com>
2016-11-04 11:36:23 -0700

    rendering 3D with seen in coffeescript

--- frontend/renderer.js
+++ frontend/renderer.js
@@ -3,12 +3,38 @@
 // All of the Node.js APIs are available in this process.
 
 var audioReceiver = require('./audio-sender');
+var coffee = require('coffee-script');
 
-var constraints = { audio: true, video: false };
-navigator.mediaDevices.getUserMedia(constraints)
-  .then(audioReceiver)
-  .catch(function(err) {
-    /* handle the error */
-    console.log("LET ME USE THE DAMN MIC!!!!");
-  }
-);
+
+function setupAudio() {
+  var constraints = { audio: true, video: false };
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then(audioReceiver)
+    .catch(function(err) {
+      /* handle the error */
+      console.log("LET ME USE THE DAMN MIC!!!!");
+    }
+  );
+}
+
+function enableCoffeescript() {
+  coffee.register();
+  require('node-jsx').install({
+    extension: '.coffee',
+    additionalTransform: function(src) {
+      return coffee.compile(src, {
+        'bare': true
+      });
+    }
+  });
+}
+
+function formUI() {
+  var renderer3D = require('./renderer3D');
+  renderer3D.render();
+};
+
+
+setupAudio();
+enableCoffeescript();
+formUI();
